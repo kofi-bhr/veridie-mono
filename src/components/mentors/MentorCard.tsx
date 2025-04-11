c24b223fbd6e1a75b90f8d1332116f80dcd5291e
Sebastian <stan@sewickley.org>
2025-04-11 14:19:49 -0400

    UI shi

--- src/components/mentors/MentorCard.tsx
+++ src/components/mentors/MentorCard.tsx
@@ -251,7 +251,7 @@ const MentorCard = ({ mentor, universities }: MentorProps) => {
 
           {/* CTA */}
           <div className="mt-auto">
-            <Button asChild className="w-full">
+            <Button asChild className="w-full rounded-none">
               <Link href={`/mentors/${slug}`}>View Profile</Link>
             </Button>
           </div>
